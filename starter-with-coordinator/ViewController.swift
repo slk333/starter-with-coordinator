57fabfb4521b6774c00657ec55ec5ea158ba2afb
slk333 <aweb33@icloud.com>
2019-05-02 00:28:47 +0200

    Initial Commit

---
+++ starter-with-coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starter-with-coordinator
+//
+//  Created by Antoine Weber on 02/05/2019.
+//  Copyright © 2019 antoine weber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
